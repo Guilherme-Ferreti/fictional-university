7da51b4b3384bfd340dcca1f70fde38cd1c322a5
Guilherme Ferreti <guiferretis@gmail.com>
2022-09-15 20:22:52 -0300

    Added parent page and child page logic

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -16,11 +16,20 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+
+        <?php
+        $parent_post_id = wp_get_post_parent_id(get_the_ID());
+
+        if ($parent_post_id) {
+        ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_post_id) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_post_id) ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php
+        }
+        ?>
 
         <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -31,7 +40,7 @@ while (have_posts()) {
         </div> -->
 
         <div class="generic-content">
-            <?php the_content(); ?>    
+            <?php the_content(); ?>
         </div>
     </div>
 <?php
