ccf9255d07429f68eb08859fd655b4c8d5926fb4
Guilherme Ferreti <guiferretis@gmail.com>
2022-09-21 14:29:44 -0300

    Added program post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -3,22 +3,38 @@
 function university_post_types() {
     register_post_type('event', [
         'show_in_rest' => true,
-        'supports' => [
-            'title', 'editor', 'excerpt',
-        ],
-        'rewrite' => [
+        'supports'     => ['title', 'editor', 'excerpt'],
+        'rewrite'      => [
             'slug' => 'events',
         ],
         'has_archive' => true,
-        'public' => true,
-        'labels' => [
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
+        'public'      => true,
+        'labels'      => [
+            'name'          => 'Events',
+            'add_new_item'  => 'Add New Event',
+            'edit_item'     => 'Edit Event',
+            'all_items'     => 'All Events',
             'singular_name' => 'Event',
         ],
         'menu_icon' => 'dashicons-calendar',
+    ]);
+
+    register_post_type('program', [
+        'show_in_rest' => true,
+        'supports'     => ['title', 'editor'],
+        'rewrite'      => [
+            'slug' => 'programs',
+        ],
+        'has_archive' => true,
+        'public'      => true,
+        'labels'      => [
+            'name'          => 'Programs',
+            'add_new_item'  => 'Add New Program',
+            'edit_item'     => 'Edit Program',
+            'all_items'     => 'All Programs',
+            'singular_name' => 'Program',
+        ],
+        'menu_icon' => 'dashicons-awards',
     ]);
 }
 
