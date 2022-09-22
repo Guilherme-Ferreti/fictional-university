7e446a08a239bf82cff91a1c7a8ef8e18c11806a
Guilherme Ferreti <guiferretis@gmail.com>
2022-09-22 20:47:14 -0300

    Added custom search results layout based on post type

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -26,18 +26,49 @@ function universitySearchResults($data) {
     while ($mainQuery->have_posts()) {
         $mainQuery->the_post();
 
-        $type = match (get_post_type()) {
-            'page', 'post' => 'generalInfo',
-            'professor'    => 'professors',
-            'program'      => 'programs',
-            'event'        => 'events',
-            'campus'       => 'campuses',
-        };
+        if (get_post_type() == 'post' or get_post_type() == 'page') {
+            $results['generalInfo'][] = [
+                'title'      => get_the_title(),
+                'permalink'  => get_the_permalink(),
+                'postType'   => get_post_type(),
+                'authorName' => get_the_author(),
+            ];
+        }
 
-        $results[$type][] = [
-            'title'     => get_the_title(),
-            'permalink' => get_the_permalink(),
-        ];
+        if (get_post_type() == 'professor') {
+            $results['professors'][] = [
+                'title'     => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image'     => get_the_post_thumbnail_url(0, 'professor-landscape'), // 0 means "current post"
+            ];
+        }
+
+        if (get_post_type() == 'program') {
+            $results['programs'][] = [
+                'title'     => get_the_title(),
+                'permalink' => get_the_permalink(),
+            ];
+        }
+
+        if (get_post_type() == 'campus') {
+            $results['campuses'][] = [
+                'title'     => get_the_title(),
+                'permalink' => get_the_permalink(),
+            ];
+        }
+
+        if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
+
+            $results['events'][] = [
+                'title'       => get_the_title(),
+                'permalink'   => get_the_permalink(),
+                'month'       => $eventDate->format('M'),
+                'day'         => $eventDate->format('d'),
+                'description' => $description,
+            ];
+        }
     }
 
     return $results;
