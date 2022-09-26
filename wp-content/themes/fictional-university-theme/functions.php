644194c352bde3788b6c7f015421a9677b05b1d8
Guilherme Ferreti <guiferretis@gmail.com>
2022-09-26 13:57:59 -0300

    Added notes CRUD

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -51,6 +51,7 @@ function university_files() {
 
     wp_localize_script('main-university-js', 'universityData', [
         'root_url' => get_site_url(), // Any variables can be informed
+        'nonce'    => wp_create_nonce('wp_rest'),
     ]);
 }
 
--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -13,30 +13,36 @@ while (have_posts()) {
     pageBanner();
 ?>
     <div class="container container--narrow page-section">
+
+        <div class="create-note">
+            <h2 class="headline headline--medium">Create New Note</h2>
+            <input class="new-note-title" placeholder="Title">
+            <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+            <span class="submit-note">Create Note</span>
+        </div>
+
         <ul class="min-list link-list" id="my-notes">
             <?php
-                $user_notes = new WP_Query([
-                    'post_type'      => 'note',
-                    'posts_per_page' => -1,
-                    'author'         => get_current_user_id(),
-                ]);
+            $userNotes = new WP_Query(array(
+                'post_type'      => 'note',
+                'posts_per_page' => -1,
+                'author'         => get_current_user_id()
+            ));
 
-                while($user_notes->have_posts()) {
-                    $user_notes->the_post();
-                ?>
-                <li>
-                    <input class="note-title-field" type="text" value="<?php echo esc_attr(get_the_title()); ?>">
+            while ($userNotes->have_posts()) {
+                $userNotes->the_post(); ?>
+                <li data-id="<?php the_ID(); ?>">
+                    <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
                     <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
                     <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
-                    <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    <textarea readonly class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
                 </li>
-                <?php
-                }
-            ?>
+            <?php } ?>
         </ul>
     </div>
-<?php
-}
+
+<?php }
 
 get_footer();
 
